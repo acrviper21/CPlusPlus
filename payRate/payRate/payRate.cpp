dca2a24423e329cbf6f667132d2462981f16dba4
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2020-05-05 01:41:57 -0500

    Add files via upload

---
+++ payRate/payRate/payRate.cpp
@@ -0,0 +1,31 @@
+// payRate.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+// By: Erik Sistos
+// Date: 20200501
+// Description: This program takes user input for the name and hours to calculate salary
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string strName = "";
+    double dblPayRate = 0.00;
+    double dblHoursWorked = 0.00;
+    double dblSalary = 0.00;
+
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, strName);
+    std::cout << "Enter the pay rate: $";
+    std::cin >> dblPayRate;
+    std::cout << "Enter the hours worked: ";
+    std::cin >> dblHoursWorked;
+    
+    dblSalary = dblPayRate * dblHoursWorked;
+
+    std::cout << "Name: " << strName << std::endl;
+    std::cout << "Pay Rate: $" << dblPayRate << std::endl;
+    std::cout << "Hours Worked: " << dblHoursWorked << std::endl;
+    std::cout << "Salary: $" << dblSalary << std::endl;
+
+}

efe69dc1dd745451d6c357bd7cfc91c040bfee99
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2020-05-05 01:50:47 -0500

    Add files via upload

--- payRate/payRate/payRate.cpp
+++ payRate/payRate/payRate.cpp
@@ -9,11 +9,13 @@
 
 int main()
 {
+    //Variables
     std::string strName = "";
     double dblPayRate = 0.00;
     double dblHoursWorked = 0.00;
     double dblSalary = 0.00;
 
+    //Gathers employees information
     std::cout << "Enter your name: ";
     std::getline(std::cin, strName);
     std::cout << "Enter the pay rate: $";
@@ -23,9 +25,11 @@ int main()
     
     dblSalary = dblPayRate * dblHoursWorked;
 
+    //Outputs employees information
     std::cout << "Name: " << strName << std::endl;
     std::cout << "Pay Rate: $" << dblPayRate << std::endl;
     std::cout << "Hours Worked: " << dblHoursWorked << std::endl;
     std::cout << "Salary: $" << dblSalary << std::endl;
-
+    
+    return 0;
 }
